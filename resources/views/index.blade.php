7cc5e3e4785a93874ab7fb48fe8561eb8725426c
Valdimiro francisco <valdimiroeinstein@gmail.com>
2020-08-08 17:50:09 +0200

    formulario levantamento completo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,18 +11,37 @@
 
 <form>
   <div class="form-group">
+      <h1 class="display-4">Deposito</h1>
+
     <label for="exampleInputEmail1">Telefone</label>
-    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="84xxxxxxx">
+    <input type="number" class="form-control" id="idnumerodep" aria-describedby="emailHelp" placeholder="84xxxxxxx">
   </div>
 
   <div class="form-group">
     <label for="exampleInputPassword1">Valor</label>
-    <input type="number" class="form-control" id="exampleInputPassword1" placeholder="0">
+    <input type="number" class="form-control" id="idvalordep" placeholder="0">
   </div>
   <button type="submit" class="btn btn-primary">Depositar</button>
 </form>
 
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Levantamento</h1>
 
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Telefone</label>
+    <input type="number" class="form-control" id="idnumero" aria-describedby="emailHelp" placeholder="84xxxxxxx">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Valor</label>
+    <input type="number" class="form-control" id="idvalor" placeholder="0">
+  </div>
+  <button type="submit" class="btn btn-primary">Levantar</button>
+</form>
+  </div>
+</div>
 
 </div>
 @endsection
